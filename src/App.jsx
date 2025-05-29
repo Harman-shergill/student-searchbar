365e26a1bf0e1d361573da1e83953bc551e81302
HARMANDEEP SINGH <132611726+Harman-shergill@users.noreply.github.com>
2025-05-29 22:42:20 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
     if (value.length >= 3) {
       if (typingTimeout) clearTimeout(typingTimeout);
       setTypingTimeout(setTimeout(() => {
-        axios.get(`https://student-search-backend.vercel.app/students?q=${value}`)
+        axios.get(`https://student-searchbackend.vercel.app/students?q=${value}`)
           .then(res => {
             setSuggestions(res.data);
           });
